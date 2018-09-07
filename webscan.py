bebd8cd1b36ad09e3a506ecd9bc8f11ad19667ac
LillyLin2016 <984137962@qq.com>
2018-09-07 15:02:05 +0800

    Add files via upload

---
+++ webscan.py
@@ -0,0 +1,73 @@
+#coding:utf-8
+#python3
+
+import os
+import sys
+import warnings
+from lib.consle_width import getTerminalSize
+import time
+
+sys.path.append(sys.path[0] + '/vuldb')
+sys.path.append(sys.path[0] + "/../")
+
+warnings.filterwarnings("ignore")    #忽略报错输出
+script_path = sys.path[0] + '/vuldb'
+
+
+
+class WebScan():
+    def __init__(self, path, url, port):
+        self.path = path
+        self.url = url
+        self.port = port
+        self.script_count = self.scan_count = self.Vulun_count = 0
+        self.console_width = getTerminalSize()[0] - 2
+        self.local_time = time.time()
+
+    def script(self):
+        for root, dirs, files in os.walk(self.path):  
+            # print(root)  #当前目录路径  
+            # print(dirs)  #当前路径下所有子目录
+            # print(files)  #当前路径下所有非目录子文件
+            return files
+
+    def scan(self):
+        TIMEOUT = 10
+        files = self.script()
+        self.script_count = len(files)
+        for script_file in files:
+            if script_file.split('.')[1] == "py":
+                script_import = script_file.split('.')[0]
+                script_res = __import__(script_import)
+                result_info = script_res.check(self.url, self.port, TIMEOUT)
+                self.scan_count += 1
+                if result_info:
+                    self.Vulun_count += 1
+                    msg = result_info
+                    self._print_msg(msg, _found_msg=True)
+                    self._print_msg()
+                    # self.outfile.write(cur_sub_domain.ljust(30) + '\t' + ips + '\n')
+                    # self.outfile.flush()
+                else : 
+                    self._print_msg()
+            else : pass
+            
+    def _print_msg(self, _msg=None, _found_msg=False):
+        if _msg is None:
+            msg = '%s Vulun| %s scanned in %.1f seconds| All %s scripts' % (
+                self.Vulun_count, self.scan_count, time.time() - start_time, self.script_count)
+            sys.stdout.write('\r' + ' ' * (self.console_width - len(msg)) + msg)
+        # elif _msg.startswith('[+] Check DNS Server'):
+            # sys.stdout.write('\r' + _msg + ' ' * (self.console_width - len(_msg)))
+        else:
+            sys.stdout.write('\r' + _msg + ' ' * (self.console_width - len(_msg)) + '\n')
+            if _found_msg:
+                msg = '%s Found| %s Groups| %s scanned in %.1f seconds' % (
+                    self.found_count, self.queue.qsize(), self.scan_count, time.time() - self.start_time)
+                sys.stdout.write('\r' + ' ' * (self.console_width - len(msg)) + msg)
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    WebScan(script_path,'127.0.0.1',8080).scan()
